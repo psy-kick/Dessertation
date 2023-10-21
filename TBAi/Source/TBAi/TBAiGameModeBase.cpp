bd1521e84d1a9c944ed3cc0da1679da38994210e
Suhas Aditya SM <suhasadityasm@gmail.com>
2023-10-21 19:30:48 +0100

    creating turn system

--- TBAi/Source/TBAi/TBAiGameModeBase.cpp
+++ TBAi/Source/TBAi/TBAiGameModeBase.cpp
@@ -3,3 +3,23 @@
 
 #include "TBAiGameModeBase.h"
 
+//constructor
+ATBAiGameModeBase::ATBAiGameModeBase()
+{
+	CurrentState = ETurnState::WaitTurn;
+}
+
+void ATBAiGameModeBase::StartTurn()
+{
+	GEngine->AddOnScreenDebugMessage(1, 5.0f, FColor::Red, TEXT("StartTurn"));
+}
+
+void ATBAiGameModeBase::WaitTurn()
+{
+	GEngine->AddOnScreenDebugMessage(1, 5.0f, FColor::Red, TEXT("WaitTurn"));
+}
+
+void ATBAiGameModeBase::EndTurn()
+{
+	GEngine->AddOnScreenDebugMessage(1, 5.0f, FColor::Red, TEXT("EndTurn"));
+}
--- TBAi/Source/TBAi/TBAiGameModeBase.h
+++ TBAi/Source/TBAi/TBAiGameModeBase.h
@@ -13,5 +13,23 @@ UCLASS()
 class TBAI_API ATBAiGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
-	
+public:
+	UENUM(BlueprintType)
+	enum class ETurnState : uint8
+	{
+		StartTurn,
+		WaitTurn,
+		EndTurn
+	};
+	ATBAiGameModeBase();
+private:
+	UPROPERTY(EditAnywhere, Category="Tracker")
+	ETurnState CurrentState;
+	UFUNCTION()
+	void StartTurn();
+	UFUNCTION()
+	void WaitTurn();
+	UFUNCTION()
+	void EndTurn();
+
 };
