89727b377346e20eeac830de632f6aa619147edd
Suhas Aditya SM <suhasadityasm@gmail.com>
2023-10-21 17:29:28 +0100

    created project

---
+++ TBAi/Source/TBAi/TBAi.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TBAi.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TBAi, "TBAi" );
---
+++ TBAi/Source/TBAi/TBAiGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TBAiGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TBAI_API ATBAiGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
